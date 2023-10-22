c4ee4fc190d6666783b621bdc57b2e229604ae2f
omebo17 <omebo17@freeuni.edu.ge>
2023-10-22 14:44:42 +0400

    change error handlings and ad a wrapper function to getCurrLocation

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable, switchMap } from 'rxjs';
 import { WeatherInfo } from 'src/app/interfaces/weather-info';
 import { WeatherService } from 'src/app/services/weather.service';
 
@@ -24,18 +25,18 @@ export class WeatherComponent {
   getWeather() {
     this.error = null;
     const cityControl = this.weatherForm.get('city');
-    if (cityControl && cityControl.value) {
+    if (cityControl?.value) {
       const city = cityControl.value;
-      this.weatherService.getWeatherByCity(city).subscribe(
-        (response) => {
+      this.weatherService.getWeatherByCity(city).subscribe({
+        next: (response) => {
           this.weatherData = response;
         },
-        (error) => {
+        error: (error) => {
           this.weatherData = null; 
-          this.error = 'Invalid input';
+          this.error = error.error.message;
           console.error('Error fetching weather data:', error);
         }
-      );
+    });
     } else {
       this.error = 'City value is null or empty'; 
     }
@@ -44,26 +45,37 @@ export class WeatherComponent {
   getCurrentLocationWeather() {
     this.error = null;
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
+      this.getLocation().pipe(
+        switchMap( (position) => {
           const latitude = position.coords.latitude;
           const longitude = position.coords.longitude;
-  
-          this.weatherService.getWeatherByCoordinates(latitude, longitude).subscribe(
-            (response) => {
-              this.weatherData = response as WeatherInfo;
-            },
-            (error) => {
-              console.error('Error fetching weather data for current location:', error);
-            }
-          );
-        },
-        (error) => {
-          console.error('Error getting current location:', error);
-        }
-      );
+          return this.weatherService.getWeatherByCoordinates(latitude, longitude)
+        })
+        ).subscribe({
+          next: (response) => {
+            this.weatherData = response as WeatherInfo;
+          },
+          error: (error) => {
+            console.error('Error fetching weather data for current location:', error);
+            this.error = error.error.message;
+          }
+        })
     } else {
       console.error('Geolocation is not supported by your browser.');
     }
   }
+
+  private getLocation(): Observable<any> {
+    return new Observable(obs => {
+     navigator.geolocation.getCurrentPosition(
+       success => {
+         obs.next(success);
+         obs.complete();
+       },
+       error => {
+         obs.error(error);
+       }
+     );
+   });
+   }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { WeatherInfo } from '../interfaces/weather-info';
 
+
+const API_URL = 'http://api.openweathermap.org';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,12 +15,12 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
 
   getWeatherByCity(city: string): Observable<WeatherInfo> {
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`;
+    const apiUrl = `${API_URL}/data/2.5/weather?q=${city}&appid=${this.apiKey}&units=metric`;
     return this.http.get<WeatherInfo>(apiUrl);
   }
 
   getWeatherByCoordinates(latitude: number, longitude: number): Observable<WeatherInfo> {
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${this.apiKey}&units=metric`;
+    const apiUrl = `${API_URL}/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${this.apiKey}&units=metric`;
     return this.http.get<WeatherInfo>(apiUrl);
   }
 
